8f079de294cc326ad223af755b04fc8d9df53ca8
arthur <arthur.tu@tpisoftware.com>
2023-04-02 21:45:11 +0800

    add swagger summary

--- Fonour.MVC/Controllers/StudentController.cs
+++ Fonour.MVC/Controllers/StudentController.cs
@@ -10,24 +10,38 @@ namespace Fonour.MVC.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        // <summary>
+        /// 取得所有學生
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public string Get()
         {
             return "Tom";
         }
 
+        /// <summary>
+        /// 新增學生
+        /// </summary>
         [HttpPost]
         public void Post()
         {
 
         }
 
+
+        /// <summary>
+        /// 修改學生訊息
+        /// </summary>
         [HttpPut]
         public void Put()
         {
 
         }
 
+        /// <summary>
+        /// 刪除學生訊息
+        /// </summary>
         [HttpDelete]
         public void Delete()
         {
